333953ea6a49a6e52b8f3944b8822758260a09c7
Christopher M. Pierce <contact@chris-pierce.com>
2025-01-21 15:44:08 -0800

    new file for constants

--- bpy_lattice/lattice.py
+++ bpy_lattice/lattice.py
@@ -8,7 +8,7 @@ from math import sin, cos, pi
 
 from bpy_lattice import slicer
 from bpy_lattice import materials
-
+from .constants import ELE_COLOR, ELE_X_SCALE, ELE_X_SCALE_FACTOR
 
 # bpy.context.scene.render.engine = 'CYCLES'
 
@@ -65,43 +65,6 @@ def import_lattice(file):
         next(f)  # Skip the header line
         lat = [map_table_dict(line) for line in f]
     return lat
-
-
-ELE_X_SCALE_FACTOR = 50
-
-
-ELE_X_SCALE = {
-    "MARKER": 0.001,
-    "E_GUN": 0.4,
-    "PIPE": 0.01,
-    "DRIFT": 0.01,
-    "ECOLLIMATOR": 0.03,
-    "RCOLLIMATOR": 0.03,
-    "SBEND": 0.02,
-    "QUADRUPOLE": 0.03,
-    "SEXTUPOLE": 0.02,
-    "LCAVITY": 0.1,
-    "RFCAVITY": 0.5,
-    "SOLENOID": 0.1,
-    "WIGGLER": 0.04,
-    "EM_FIELD": 0.1,
-    "INSTRUMENT": 0.05,
-}
-
-
-ELE_COLOR = {
-    "PIPE": (1, 1, 1),
-    "E_GUN": (0.5, 0.5, 0.5),
-    "SBEND": (1, 0, 0),
-    "QUADRUPOLE": (0, 0, 1),
-    "LCAVITY": (0, 1, 0),
-    "RFCAVITY": (0, 1, 0),
-    "SOLENOID": (1, 0, 1),  # purple
-    "EM_FIELD": (1, 0, 0.5),  # ??
-    "SEXTUPOLE": (1, 1, 0),  # yellow
-    "WIGGLER": (1, 0.4, 0),  # orange?
-    "INSTRUMENT": (0, 0, 0),
-}  # black
 
 
 def ele_x_scale(ele):
