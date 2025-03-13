34f9eba6143f2a55970fc088bc52da5351dfc4f8
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2025-03-13 14:03:41 -0700

    REF: make install_blender_package just for bpy-lattice

--- scripts/install_blender_package.sh
+++ scripts/install_blender_package.sh
@@ -14,25 +14,24 @@ list_blender_versions() {
 show_help() {
   echo "Usage: $0 <blender_version> <path_to_local_package> [--editable]"
   echo
-  echo "This script installs a local Python package into Blender's Python environment."
+  echo "This script installs bpy-lattice into Blender's Python environment."
   echo
   echo "Arguments:"
   echo "  <blender_version>          The version of Blender to target (e.g., 4.2)."
-  echo "  <path_to_local_package>    The path to the local Python package to install."
   echo "  [--editable]               Optional flag to install the package in editable mode."
   echo
   echo "Base Path: $BASE_PATH"
   echo "Modules Path: $MODULES_PATH"
   echo
   echo "Examples:"
-  echo "  $0 4.2 /path/to/your/package"
-  echo "  $0 4.2 /path/to/your/package --editable"
+  echo "  $0 4.2"
+  echo "  $0 4.2 --editable"
   echo
   echo "This script also lists available Blender versions if arguments are missing or incorrect."
 }
 
 # Check if at least two arguments are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ -z "$1" ]; then
   BASE_PATH="/Users/$(whoami)/Library/Application Support/Blender"
   MODULES_PATH="$BASE_PATH/<version>/scripts/modules"
   show_help
@@ -41,10 +40,14 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 
 BLENDER_VERSION=$1
-PACKAGE_PATH=$2
-EDITABLE_FLAG=$3
+EDITABLE_FLAG=$2
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+GIT_SOURCE_DIR=$(cd $SCRIPT_DIR/.. && pwd)
+BPY_LATTICE_PACKAGE_DIR="$GIT_SOURCE_DIR/bpy_lattice"
 BASE_PATH="/Users/$(whoami)/Library/Application Support/Blender/$BLENDER_VERSION"
 MODULES_PATH="$BASE_PATH/scripts/modules"
+DEST_MODULE_PATH="$BASE_PATH/scripts/modules/bpy_lattice"
 
 # Check if the base path exists
 if [ ! -d "$BASE_PATH" ]; then
@@ -65,13 +68,17 @@ fi
 # Create the modules directory if it doesn't exist
 mkdir -p "$MODULES_PATH"
 
-# Determine the install command
-INSTALL_BASE_COMMAND="$PYTHON_EXEC -m pip install --no-deps --ignore-installed --target=\"$MODULES_PATH\""
+# Clean previous installations
+if [[ -d "$DEST_MODULE_PATH" || -L "$DEST_MODULE_PATH" ]]; then
+  rm -rf "$DEST_MODULE_PATH"* "$MODULES_PATH/bin/bmad-to-blender"
+  echo "* Cleaned previous installation of bpy-lattice"
+fi
 
-if [ "$EDITABLE_FLAG" == "--editable" ]; then
-  INSTALL_COMMAND="$INSTALL_BASE_COMMAND -e \"$PACKAGE_PATH\""
+# Determine the install command
+if [ "$EDITABLE_FLAG" == "--editable" ] || [ "$EDITABLE_FLAG" == "-e" ]; then
+  INSTALL_COMMAND="ln -sf \"$BPY_LATTICE_PACKAGE_DIR\" \"$MODULES_PATH\""
 else
-  INSTALL_COMMAND="$INSTALL_BASE_COMMAND \"$PACKAGE_PATH\""
+  INSTALL_COMMAND="$PYTHON_EXEC -m pip install --no-deps --upgrade --target=\"$MODULES_PATH\" \"$GIT_SOURCE_DIR\""
 fi
 
 echo "Installing with command: $INSTALL_COMMAND"
