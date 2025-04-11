95974492c3f08826cd0900351de0b4c1f040ee82
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2025-04-11 16:19:30 -0700

    add_tracks_to_blender and more organized collections

--- bpy_lattice/blend.py
+++ bpy_lattice/blend.py
@@ -4,7 +4,8 @@ import pathlib
 
 import bpy
 
-from .elements import AnyElement
+from .elements import AnyElement, Bend, Undulator
+from .tracks import Track
 from .objects import add_children_from_blend, remap_axes
 
 
@@ -38,8 +39,12 @@ def add_elements_to_blender(
     if remove_unused:
         remove_unused_data()
 
+    elements_collection_name = "Elements"
+    elements_collection = bpy.data.collections.new(elements_collection_name)
+    bpy.context.scene.collection.children.link(elements_collection)
+
     library_cache = {}
-    library_collection_name = "library"
+    library_collection_name = "Library"
     library_collection = bpy.data.collections.new(library_collection_name)
     bpy.context.scene.collection.children.link(library_collection)
 
@@ -54,9 +59,10 @@ def add_elements_to_blender(
             collection = bpy.data.collections[collection_name]
         except KeyError:
             collection = bpy.data.collections.new(collection_name)
-            bpy.context.scene.collection.children.link(collection)
+            # bpy.context.scene.collection.children.link(collection)
 
-        obj = ele.to_object()
+            # Link under Elements
+            elements_collection.children.link(collection)
 
         bfile = None
         if ele.cad_model:
@@ -66,15 +72,27 @@ def add_elements_to_blender(
                 bfile = catalogue / ele.cad_model
 
             if bfile.exists():
-                print(f"Blend file exists: {bfile}")
+                print(f"✅ Blend file exists: {bfile}")
+                obj = ele.to_empty_object()
+                obj.empty_display_type = "ARROWS"
+                obj.empty_display_size = 0.1
                 add_children_from_blend(
                     obj,
                     bfile,
                     library_cache,
                     collection=library_collection,
                 )
+                if isinstance(ele, (Bend, Undulator)):
+                    aperture_obj = ele.aperture_object()
+                    if aperture_obj is not None:
+                        aperture_obj.parent = obj
+
             else:
-                print(f"--- MISSING Blend file: {bfile}")
+                print(f"⛔️ MISSING Blend file: {bfile}")
+                obj = ele.to_object()
+
+        else:
+            obj = ele.to_object()
 
         objs.append(obj)
 
@@ -87,6 +105,18 @@ def add_elements_to_blender(
     return library_cache, objs
 
 
-def remamp_axes():
-    parentless_objects = [obj for obj in bpy.data.objects if obj.parent is None]
+def add_tracks_to_blender(tracks: list[Track], collection=None):
+    if collection is None:
+        collection = bpy.data.collections.new("Tracks")
+        bpy.context.scene.collection.children.link(collection)
+    for track in tracks:
+        curve = track.to_curve()
+        collection.objects.link(curve)
+
+
+def remamp_axes(objects=None):
+    if objects is None:
+        objects = bpy.data.objects
+    print(f"🟣 remapping {len(objects)} objects Z->X")
+    parentless_objects = [obj for obj in objects if obj.parent is None]
     remap_axes(parentless_objects, x_axis="Z", y_axis="X", z_axis="Y")
