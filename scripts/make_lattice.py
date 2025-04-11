1e47f755133f1df352d92cca3e4c2b655f026f03
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2025-04-11 16:20:24 -0700

    Add tracks to make lattice script

--- scripts/make_lattice.py
+++ scripts/make_lattice.py
@@ -8,11 +8,14 @@ from pathlib import Path
 import importlib
 
 from bpy_lattice.elements import load_elements_from_json
+from bpy_lattice.tracks import load_tracks_from_json
+
 
 importlib.reload(bpy_lattice)
 importlib.reload(bpy_lattice.mesh)
 importlib.reload(bpy_lattice.objects)
 importlib.reload(bpy_lattice.elements)
+importlib.reload(bpy_lattice.tracks)
 importlib.reload(bpy_lattice.blend)
 
 
@@ -23,5 +26,12 @@ assert JSON_FILE.exists()
 eles = load_elements_from_json(JSON_FILE)
 
 bpy_lattice.blend.add_elements_to_blender(eles)
+
+
+# Tracks
+tracks_file = Path("tracks.json")
+if tracks_file.exists():
+    tracks = load_tracks_from_json(tracks_file)
+    bpy_lattice.blend.add_tracks_to_blender(tracks)
 
 bpy_lattice.blend.remamp_axes()
